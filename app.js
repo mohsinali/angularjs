5d567a643831ea3c1d20b4c654cc1897d9d7731f
Mohsin <mohammadmohsin@gmail.com>
2014-12-29 16:11:02 +0500

    Move directives to new file.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function() {
-  var app = angular.module('gemStore', []);
+  var app = angular.module('gemStore', ['store-directives']);
 
   app.controller('GalleryController', function(){
     this.current = 0;
@@ -35,26 +35,26 @@
 
   });
 
-  app.directive("productDescription", function() {
-    return {
-      restrict: 'E',
-      templateUrl: "product-description.html"
-    };
-  });
+  // app.directive("productDescription", function() {
+  //   return {
+  //     restrict: 'E',
+  //     templateUrl: "product-description.html"
+  //   };
+  // });
 
-  app.directive("productReviews", function() {
-    return {
-      restrict: 'E',
-      templateUrl: "product-reviews.html"
-    };
-  });
+  // app.directive("productReviews", function() {
+  //   return {
+  //     restrict: 'E',
+  //     templateUrl: "product-reviews.html"
+  //   };
+  // });
 
-  app.directive('productSpecs', function(){
-    return {
-      restrict: 'A',
-      templateUrl: 'product-specs.html'
-    };
-  });
+  // app.directive('productSpecs', function(){
+  //   return {
+  //     restrict: 'A',
+  //     templateUrl: 'product-specs.html'
+  //   };
+  // });
 
   
 
