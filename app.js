870a6748cc69f40b9f1a75487f79aa4283b8b53d
Mohsin <mohammadmohsin@gmail.com>
2014-12-30 11:22:10 +0500

    Call services using angular.

--- app.js
+++ app.js
@@ -35,28 +35,6 @@
 
   });
 
-  // app.directive("productDescription", function() {
-  //   return {
-  //     restrict: 'E',
-  //     templateUrl: "product-description.html"
-  //   };
-  // });
-
-  // app.directive("productReviews", function() {
-  //   return {
-  //     restrict: 'E',
-  //     templateUrl: "product-reviews.html"
-  //   };
-  // });
-
-  // app.directive('productSpecs', function(){
-  //   return {
-  //     restrict: 'A',
-  //     templateUrl: 'product-specs.html'
-  //   };
-  // });
-
-  
 
   var gems = [
     {
@@ -132,3 +110,13 @@
     }
   ];
 })();
+
+
+// Some services code
+/*
+app.controller('productsController', ['$http', function($http){
+  $http.get('/store-products.html').success(function(data){
+
+  });
+}]);
+*/
