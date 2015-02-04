568f598d5e248d3c394091eccc2e1a4c0c051008
Mohsin <mohammadmohsin@gmail.com>
2015-02-04 13:04:30 +0500

    New todo and country list.

--- todo/todo_2.js
+++ todo/todo_2.js
@@ -4,7 +4,8 @@
   app.controller('TodoController', function(){
     this.todos = [
       {text: 'Apply some js code.', done: false},
-      {text: 'Have some food.', done: false}
+      {text: 'Have some food.', done: false},
+      {text: 'Bring printed application', done: false}
     ];
 
     this.countries = [
@@ -12,5 +13,23 @@
       {id: '2', text: 'China'},
       {id: '3', text: 'India'}
     ];
+
+    this.remaining = function(){
+      var count = 0;
+      angular.forEach(this.todos, function(todo){
+        count += todo.done ? 0 : 1;
+      });
+      return count;
+    };
+
+    this.addTodo = function(){
+      this.todos.push({text: this.todoText, done: false});
+      this.todoText = '';
+    };
+
+    this.addCountry = function(){
+      this.countries.push({id: 4, text: this.countryText});
+      this.countryText = '';
+    };
   });
 })();
