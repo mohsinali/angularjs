10190076444a8d3b0bdb5f7e21a8fe4a3193c270
Mohsin Ali <mohammadmohsin@gmail.com>
2015-05-11 10:52:42 +0500

    New index page.

--- wrangler_note/js/app.js
+++ wrangler_note/js/app.js
@@ -1,1 +1,6 @@
-var app = angular.module('NoteWrangler', ['ngRoute']);
+angular.module('NoteWrangler', ['ngRoute'])
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/notes', {
+    templateUrl: 'templates/pages/notes/index.html'
+  });
+ }]);
